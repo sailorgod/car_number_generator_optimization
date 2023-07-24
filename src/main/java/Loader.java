af19b8ccd94fb1413492601f9f3bb8c815b4977b
@evgenii_iurin <mmun6518@gmail.com>
2023-07-24 14:01:29 +0300

    delete class Concatenatoin, create class TaskWriter, update Loader

--- src/main/java/Loader.java
+++ src/main/java/Loader.java
@@ -1,41 +1,18 @@
-import java.io.FileOutputStream;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Loader {
 
-    public static void main(String[] args) throws Exception {
-        long start = System.currentTimeMillis();
+    public static void main ( String[] args ) throws Exception {
 
-        FileOutputStream writer = new FileOutputStream("resources/numbers.txt");
+        ThreadPoolExecutor executor = ( ThreadPoolExecutor ) Executors.newFixedThreadPool ( 5 );
 
-        char[] letters = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
-        for (int number = 1; number < 1000; number++) {
-            int regionCode = 199;
-            for (char firstLetter : letters) {
-                for (char secondLetter : letters) {
-                    for (char thirdLetter : letters) {
-                        String carNumber = firstLetter + padNumber(number, 3) +
-                            secondLetter + thirdLetter + padNumber(regionCode, 2);
-                        writer.write(carNumber.getBytes());
-                        writer.write('\n');
-                    }
-                }
-            }
+        for ( int i = 0 ; i < executor.getMaximumPoolSize ( ) ; i++ ) {
+            String threadNumber = Integer.toString ( i );
+            executor.execute ( new TaskWriter ( threadNumber ) );
         }
+        executor.shutdown ( );
 
-        writer.flush();
-        writer.close();
-
-        System.out.println((System.currentTimeMillis() - start) + " ms");
     }
 
-    private static String padNumber(int number, int numberLength) {
-        String numberStr = Integer.toString(number);
-        int padSize = numberLength - numberStr.length();
-
-        for (int i = 0; i < padSize; i++) {
-            numberStr = '0' + numberStr;
-        }
-
-        return numberStr;
-    }
 }
